b93d52bbb63b0373c713890dae6860aeb5239c19
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-12-16 17:39:20 +0900

    add ask My PDF feature

--- pdf_knowledge_base.py
+++ pdf_knowledge_base.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from qdrant import build_vector_store
+from qdrant import build_qa_model, build_vector_store
+from langchain.callbacks import get_openai_callback
 
 
 def handle_pdf_knowledge_base(llm):
@@ -9,7 +10,7 @@ def handle_pdf_knowledge_base(llm):
     if selection == "PDF Upload":
         page_pdf_upload_and_build_vector_db()
     elif selection == "Ask My PDF(s)":
-        page_ask_my_pdf()
+        page_ask_my_pdf(llm)
 
     costs = st.session_state.get("costs", [])
     st.sidebar.markdown("## Costs")
@@ -18,9 +19,28 @@ def handle_pdf_knowledge_base(llm):
         st.sidebar.markdown(f"- ${cost:.5f}")
 
 
-def page_ask_my_pdf():
+def page_ask_my_pdf(llm):
     st.title("Ask My PDF(s)")
-    st.write("Under Construction")
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        query = st.text_input("Query: ", key="input")
+        if not query:
+            answer = None
+        else:
+            qa = build_qa_model(llm)
+            if qa:
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = ask(qa, query)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+        if answer:
+            with response_container:
+                st.markdown("## Answer")
+                st.write(answer)
 
 
 def page_pdf_upload_and_build_vector_db():
@@ -33,19 +53,27 @@ def page_pdf_upload_and_build_vector_db():
                 build_vector_store(pdf_text)
 
 
+def ask(qa, query):
+    with get_openai_callback() as cb:
+        # query / result / source_documents
+        answer = qa(query)
+
+    return answer, cb.total_cost
+
+
 def get_pdf_text():
     uploaded_file = st.file_uploader(
-        label="Upload your PDF here", type="pdf"  # アップロードを許可する拡張子 (複数設定可)
+        label="Upload your PDF here😇", type="pdf"  # アップロードを許可する拡張子 (複数設定可)
     )
     if uploaded_file:
         pdf_reader = PdfReader(uploaded_file)
         text = "\n\n".join([page.extract_text() for page in pdf_reader.pages])
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            model_name=st.session_state.emb_model_name,
+            model_name="text-embedding-ada-002",
             # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
             # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
             # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
-            chunk_size=250,
+            chunk_size=500,
             chunk_overlap=0,
         )
         return text_splitter.split_text(text)
